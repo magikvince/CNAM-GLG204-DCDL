e9ae9170883cc5cd24a04c1191c50cc15357abbc
magikvince <vincent1603@hotmail.com>
2022-07-12 00:21:24 +0200

    modified Player class and register.html form to add new fields

--- src/main/java/fr/magikvince/dcdl/security/Player.java
+++ src/main/java/fr/magikvince/dcdl/security/Player.java
@@ -1,10 +1,17 @@
 package fr.magikvince.dcdl.security;
 
+import java.util.Date;
+
 public class Player extends Person {
-	
 	
 	private String firstname;
 	private String lastname;
+	private String email;
+	private String password;
+	private Date birthdate;
+	private String country;
+	private String city;
+	
 
 	public Player(String pseudo) {
 		super(pseudo);
@@ -30,6 +37,48 @@ public class Player extends Person {
 	public void setLastname(String lastname) {
 		this.lastname = lastname;
 	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Date getBirthdate() {
+		return birthdate;
+	}
+
+	public void setBirthdate(Date birthdate) {
+		this.birthdate = birthdate;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+	
 
 	
 }
