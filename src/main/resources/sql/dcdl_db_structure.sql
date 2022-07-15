74b2adb60957d1d4effc5ab585e186d65cb737c4
magikvince <vincent1603@hotmail.com>
2022-07-16 00:50:52 +0200

    added auto_increment for id_player in T_PLAYER table

--- src/main/resources/sql/dcdl_db_structure.sql
+++ src/main/resources/sql/dcdl_db_structure.sql
@@ -3,13 +3,16 @@ use dcdl;
 
 DROP TABLE if exists T_PLAYER CASCADE;
 CREATE TABLE T_PLAYER (
- id_player NUMERIC(8) NOT NULL PRIMARY KEY,
+ id_player INT(8) NOT NULL PRIMARY KEY AUTO_INCREMENT,
  pseudo VARCHAR(30),
  firstname VARCHAR(30),
  lastname VARCHAR(30),
- email VARCHAR(50),
+ email VARCHAR(50) UNIQUE,
  password VARCHAR(50),
  birthdate DATETIME,
  country VARCHAR(40), 
  city VARCHAR(40)
 );
+
+INSERT INTO `dcdl`.`t_player` (`pseudo`,`firstname`,`lastname`,`email`,`password`,`birthdate`,`country`,`city`) VALUES ('magikvince','Vincent', 'NAVARRO','vincent1603@hotmail.com', 'admin', '1978-03-16', 'FRANCE', 'MELUN');
+INSERT INTO `dcdl`.`t_player` (`pseudo`,`firstname`,`lastname`,`email`,`password`,`birthdate`,`country`,`city`) VALUES ('sayurisan','LESLIE', 'SAYURI','sayuri@hotmail.com', 'admin', '1974-05-30', 'FRANCE', 'MELUN');
