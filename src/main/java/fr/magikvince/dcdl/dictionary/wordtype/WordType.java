594676a983b3e14d95a77c4dce24c96b05b22e0a
magikvince <vincent1603@hotmail.com>
2022-07-28 00:20:53 +0200

    added attribute Language to class WordType and added getters / setters

--- src/main/java/fr/magikvince/dcdl/dictionary/wordtype/WordType.java
+++ src/main/java/fr/magikvince/dcdl/dictionary/wordtype/WordType.java
@@ -7,6 +7,8 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import fr.magikvince.dcdl.dictionary.language.Language;
+
 @Entity
 @Table(name="T_WORDTYPE")
 public class WordType {
@@ -14,11 +16,35 @@ public class WordType {
 	@Id
 	private int idWordType;
 	private String wordType;
+	private Language language;
 	
 	public WordType()
 	{
 		
 	}
-	
+
+	public int getIdWordType() {
+		return idWordType;
+	}
+
+	public void setIdWordType(int idWordType) {
+		this.idWordType = idWordType;
+	}
+
+	public String getWordType() {
+		return wordType;
+	}
+
+	public void setWordType(String wordType) {
+		this.wordType = wordType;
+	}
+
+	public Language getLanguage() {
+		return language;
+	}
+
+	public void setLanguage(Language language) {
+		this.language = language;
+	}
 	
 }
