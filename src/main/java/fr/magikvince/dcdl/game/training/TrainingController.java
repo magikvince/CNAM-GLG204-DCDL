a6207034728255d100a7a11a68fb24349aa52f76
magikvince <vincent1603@hotmail.com>
2022-08-10 01:00:23 +0200

    fixed template and controller for Training count, added @Service annotation

--- src/main/java/fr/magikvince/dcdl/game/training/TrainingController.java
+++ src/main/java/fr/magikvince/dcdl/game/training/TrainingController.java
@@ -4,12 +4,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
 public class TrainingController {
 
+	@Autowired
+	TrainingService trainingservice;
+	
 	@GetMapping("/training")
 	public String trainingGET(Model model)
 	{
@@ -27,6 +32,14 @@ public class TrainingController {
 	public String trainingCountGET(Model model)
 	{
 		model.addAttribute("drawnumber", Integer.valueOf(0));
+		return "game/training-count.html";
+	}
+	
+	@PostMapping("/training/count")
+	public String trainingCountPOST(@RequestParam int number , Model model)
+	{
+		model.addAttribute("drawnumber", number);
+		System.out.println("drawnumber entered by user: " + number);
 		return "game/training-count.html";
 	}
 	
--- src/main/java/fr/magikvince/dcdl/game/training/TrainingService.java
+++ src/main/java/fr/magikvince/dcdl/game/training/TrainingService.java
@@ -1,8 +1,11 @@
 package fr.magikvince.dcdl.game.training;
 
+import org.springframework.stereotype.Service;
+
 import fr.magikvince.dcdl.game.draw.DrawCount;
 import fr.magikvince.dcdl.game.draw.DrawLetter;
 
+@Service
 public class TrainingService {
 
 	
