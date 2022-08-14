2b6f1d541ec9afcc0a69ccde193ffebac6587471
magikvince <vincent1603@hotmail.com>
2022-08-14 21:39:03 +0200

    fixed the form in training-count template and the TrainingController , deleted the RequestParam use

--- src/main/java/fr/magikvince/dcdl/game/training/TrainingController.java
+++ src/main/java/fr/magikvince/dcdl/game/training/TrainingController.java
@@ -1,10 +1,14 @@
 package fr.magikvince.dcdl.game.training;
 
+import java.net.http.HttpRequest;
+import java.util.Enumeration;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -32,17 +36,20 @@ public class TrainingController {
 	public String trainingCountGET(Model model)
 	{
 		model.addAttribute("drawnumber", Integer.valueOf(0));
+		
 		return "game/training-count.html";
 	}
 	
 	@PostMapping("/training/count")
-	public String trainingCountPOST(@RequestParam int number , Model model)
+	public String trainingCountPOST(Integer drawnumber, Model model)
 	{
-		model.addAttribute("drawnumber", number);
-		System.out.println("drawnumber entered by user: " + number);
+		model.addAttribute("drawnumber", drawnumber);
+		
+		//appel du service avec le drawnumber pour obtenir une collection de DrawCount ...
+		// puis ajout de cette collection et renvoie sur le template pour demarrage du jeu et affichage !!!
 		return "game/training-count.html";
 	}
-	
+		
 	@PostMapping("/training")
 	public String trainingPOST(Model model)
 	{
