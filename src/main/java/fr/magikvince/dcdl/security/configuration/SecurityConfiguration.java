1d3608510c44b3f96c93bf3c4b0b353d1152adbe
magikvince <vincent1603@hotmail.com>
2022-08-18 22:29:18 +0200

    added a configuration sub package for security, duplicated SecurityConfiguration to SecurityConfigurationDeprecated and created new methods with @bean in original one

--- src/main/java/fr/magikvince/dcdl/security/configuration/SecurityConfiguration.java
+++ src/main/java/fr/magikvince/dcdl/security/configuration/SecurityConfiguration.java
@@ -1,27 +1,55 @@
 package fr.magikvince.dcdl.security.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
-@SuppressWarnings("deprecation")
 @Configuration
-public class SecurityConfiguration extends  WebSecurityConfigurerAdapter
-{
+public class SecurityConfiguration {
 
-	  @Override
-	  protected void configure(HttpSecurity http) throws Exception {
-	        http.authorizeRequests()
-	                .antMatchers("/logon").permitAll()
-	                .anyRequest().authenticated()
-	                .and()
-	                .formLogin().loginPage("/logon");
-	    }
-	 
-	  @Override
-	  public void configure(WebSecurity web) throws Exception {
-	        web.ignoring().antMatchers("/webjars/**", "/images/**", "/css/**");
-	  }
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        
+    	String[] staticResources = { "/img/**"};
+    	
+    	http.csrf().and().formLogin().loginPage("/logon")				// the custom login page
+        	.defaultSuccessUrl("/")				// the landing page after a successful login
+        	.and()								// possible : failureUrl() : the landing page after an unsuccessful login
+        .logout()								// must be a POST with csrf on
+        	.logoutSuccessUrl("/")
+        	.and()
+        .authorizeRequests()
+        	.antMatchers(staticResources).permitAll()
+        	.antMatchers("/", "/logon").permitAll()
+        	.anyRequest().authenticated();
+    	
+    	return http.build();
+    	
+    }
+    
+    @Bean
+    public UserDetailsService userDetailsService() {
+            UserDetails user = User.withDefaultPasswordEncoder()
+                    .username("admin@gmail.com")
+                    .password("admin")
+                    .roles("USER")
+                    .build();
+            return new InMemoryUserDetailsManager(user);
+    }
+    
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
+
---
+++ src/main/java/fr/magikvince/dcdl/security/configuration/SecurityConfigurationDeprecated.java
@@ -0,0 +1,28 @@
+package fr.magikvince.dcdl.security.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+//@SuppressWarnings("deprecation")
+//@Configuration
+public class SecurityConfigurationDeprecated extends  WebSecurityConfigurerAdapter
+{
+
+	  @Override
+	  protected void configure(HttpSecurity http) throws Exception {
+	        http.authorizeRequests()
+	                .antMatchers("/logon").permitAll()
+	                .anyRequest().authenticated()
+	                .and()
+	                .formLogin().loginPage("/logon");
+	    }
+	 
+	  @Override
+	  public void configure(WebSecurity web) throws Exception {
+	        web.ignoring().antMatchers("/webjars/**", "/images/**", "/css/**");
+	  }
+}
+
