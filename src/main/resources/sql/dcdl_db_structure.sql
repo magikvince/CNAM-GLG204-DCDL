77dfc2b73c7d656a952d01d76f1e9052357cfc3a
magikvince <vincent1603@hotmail.com>
2022-08-22 23:19:14 +0200

    deleted pseudo from T_PLAYER and modified insert queries, added a field isenable in T_USER and in insert queries

--- src/main/resources/sql/dcdl_db_structure.sql
+++ src/main/resources/sql/dcdl_db_structure.sql
@@ -5,7 +5,6 @@ DROP TABLE if exists T_PLAYER CASCADE;
 CREATE TABLE T_PLAYER (
  id_player INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
  id_user_fk INT,
- pseudo VARCHAR(30) NOT NULL,
  league_rank INT,
  league_score INT
 );
@@ -13,6 +12,7 @@ CREATE TABLE T_PLAYER (
 DROP TABLE if exists T_USER CASCADE;
 CREATE TABLE T_USER (
  id_user INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+ isenable BOOLEAN,
  pseudo VARCHAR(30),
  firstname VARCHAR(30),
  lastname VARCHAR(30),
@@ -23,8 +23,8 @@ CREATE TABLE T_USER (
  city VARCHAR(40)
 );
 
-INSERT INTO T_USER (`pseudo`,`firstname`,`lastname`,`email`,`password`,`birthdate`,`country`,`city`) VALUES ('magikvince','Vincent', 'NAVARRO','vincent1603@hotmail.com', 'admin', '1978-03-16', 'FRANCE', 'MELUN');
-INSERT INTO T_USER (`pseudo`,`firstname`,`lastname`,`email`,`password`,`birthdate`,`country`,`city`) VALUES ('sayurisan','LESLIE', 'SAYURI','sayuri@hotmail.com', 'admin', '1974-05-30', 'FRANCE', 'MELUN');
+INSERT INTO T_USER (true, `pseudo`,`firstname`,`lastname`,`email`,`password`,`birthdate`,`country`,`city`) VALUES ('magikvince','Vincent', 'NAVARRO','vincent1603@hotmail.com', 'admin', '1978-03-16', 'FRANCE', 'MELUN');
+INSERT INTO T_USER (true, `pseudo`,`firstname`,`lastname`,`email`,`password`,`birthdate`,`country`,`city`) VALUES ('sayurisan','LESLIE', 'SAYURI','sayuri@hotmail.com', 'admin', '1974-05-30', 'FRANCE', 'MELUN');
 
 ALTER TABLE `dcdl`.`t_player` 
 ADD INDEX `id_user_fk_idx` (`id_user_fk` ASC) VISIBLE;
@@ -34,6 +34,6 @@ ADD CONSTRAINT `id_user_fk`
   FOREIGN KEY (`id_user_fk`)
   REFERENCES `dcdl`.`t_user` (`id_user`);
 
-INSERT INTO `dcdl`.`t_player` (`id_user_fk`,`pseudo`,`league_rank`,`league_score`) VALUES(1,'magikvince',0,0);
-INSERT INTO `dcdl`.`t_player` (`id_user_fk`,`pseudo`,`league_rank`,`league_score`) VALUES(2,'sayurisan',0,0);
+INSERT INTO `dcdl`.`t_player` (`id_user_fk`,`league_rank`,`league_score`) VALUES(1,0,0);
+INSERT INTO `dcdl`.`t_player` (`id_user_fk`,`league_rank`,`league_score`) VALUES(2,0,0);
 
