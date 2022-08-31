6c409bb60da545ad002ca0a0eb9b6ef5463f4736
magikvince <vincent1603@hotmail.com>
2022-09-01 01:01:21 +0200

    added a drop table for T_ROLES, fixed insert queries for field is_enabled

--- src/main/resources/sql/dcdl_db_structure.sql
+++ src/main/resources/sql/dcdl_db_structure.sql
@@ -9,6 +9,13 @@ CREATE TABLE T_PLAYER (
  league_score INT
 );
 
+DROP TABLE if exists T_ROLES CASCADE;
+CREATE TABLE IF NOT EXISTS T_ROLES (
+ id_role INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+ id_user_fk INT,
+ role VARCHAR(45) NOT NULL
+);
+
 DROP TABLE if exists T_USER CASCADE;
 CREATE TABLE T_USER (
  id_user INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -24,11 +31,6 @@ CREATE TABLE T_USER (
  city VARCHAR(40)
 );
 
-CREATE TABLE IF NOT EXISTS T_ROLES (
- id_role INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
- id_user_fk INT,
- role VARCHAR(45) NOT NULL
-);
 
 ALTER TABLE `dcdl`.`t_player` 
 ADD INDEX `id_user_fk_idx` (`id_user_fk` ASC) VISIBLE;
@@ -48,8 +50,8 @@ ADD CONSTRAINT `id_user_fk_roles`
   REFERENCES `dcdl`.`t_user` (`id_user`);
 
 
-INSERT INTO T_USER (`is_enable`, `creation_datetime`, `pseudo`,`firstname`,`lastname`,`email`,`password`,`birthdate`,`country`,`city`) VALUES (true, sysdate(), 'magikvince','Vincent', 'NAVARRO','vincent1603@hotmail.com', 'admin', '1978-03-16', 'FRANCE', 'MELUN');
-INSERT INTO T_USER (`is_enable`, `creation_datetime`, `pseudo`,`firstname`,`lastname`,`email`,`password`,`birthdate`,`country`,`city`) VALUES (true, sysdate(), 'sayurisan','LESLIE', 'SAYURI','sayuri@hotmail.com', 'admin', '1974-05-30', 'FRANCE', 'MELUN');
+INSERT INTO T_USER (`is_enabled`, `creation_datetime`, `pseudo`,`firstname`,`lastname`,`email`,`password`,`birthdate`,`country`,`city`) VALUES (true, sysdate(), 'magikvince','Vincent', 'NAVARRO','vincent1603@hotmail.com', 'admin', '1978-03-16', 'FRANCE', 'MELUN');
+INSERT INTO T_USER (`is_enabled`, `creation_datetime`, `pseudo`,`firstname`,`lastname`,`email`,`password`,`birthdate`,`country`,`city`) VALUES (true, sysdate(), 'sayurisan','LESLIE', 'SAYURI','sayuri@hotmail.com', 'admin', '1974-05-30', 'FRANCE', 'MELUN');
 
 INSERT INTO `dcdl`.`t_player` (`id_user_fk`,`league_rank`,`league_score`) VALUES(1,0,0);
 INSERT INTO `dcdl`.`t_player` (`id_user_fk`,`league_rank`,`league_score`) VALUES(2,0,0);
