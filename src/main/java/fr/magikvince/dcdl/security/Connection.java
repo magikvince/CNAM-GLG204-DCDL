c56749294b7f9342b8126f9799360c2f1d7b9ea2
magikvince <vincent1603@hotmail.com>
2022-09-05 23:20:29 +0200

    added methods and attributes for Connection class

--- src/main/java/fr/magikvince/dcdl/security/Connection.java
+++ src/main/java/fr/magikvince/dcdl/security/Connection.java
@@ -3,6 +3,10 @@ package fr.magikvince.dcdl.security;
 import java.time.LocalDateTime;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import fr.magikvince.dcdl.security.user.User;
@@ -10,8 +14,47 @@ import fr.magikvince.dcdl.security.user.User;
 @Entity
 @Table(name = "T_CONNECTION")
 public class Connection {
+	
+	@Id
 	int idConnection;
+	
 	LocalDateTime dtLogon;
 	LocalDateTime dtLogout;
+	
+	@ManyToOne
+	@JoinColumn(name="id_user_fk")
 	User user;
+	
+	public Connection()
+	{
+		
+	}
+	
+	
+	public int getIdConnection() {
+		return idConnection;
+	}
+	public void setIdConnection(int idConnection) {
+		this.idConnection = idConnection;
+	}
+	public LocalDateTime getDtLogon() {
+		return dtLogon;
+	}
+	public void setDtLogon(LocalDateTime dtLogon) {
+		this.dtLogon = dtLogon;
+	}
+	public LocalDateTime getDtLogout() {
+		return dtLogout;
+	}
+	public void setDtLogout(LocalDateTime dtLogout) {
+		this.dtLogout = dtLogout;
+	}
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+	
 }
