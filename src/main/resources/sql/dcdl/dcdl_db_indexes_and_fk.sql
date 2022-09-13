865d0e0f0ba3bd2785a57e54c7e805d2829f6c0b
magikvince <vincent1603@hotmail.com>
2022-09-14 00:18:49 +0200

    added constraints for tables games, solution, solution_details, association tables...

--- src/main/resources/sql/dcdl/dcdl_db_indexes_and_fk.sql
+++ src/main/resources/sql/dcdl/dcdl_db_indexes_and_fk.sql
@@ -39,3 +39,68 @@ ADD INDEX `id_room_idx` (`id_room` ASC) VISIBLE;
 ALTER TABLE `dcdl`.`t_league` 
 ADD INDEX `id_league_idx` (`id_league` ASC) VISIBLE;
 
+
+ALTER TABLE `dcdl`.`t_game` 
+ADD INDEX `id_game_idx` (`id_game` ASC) VISIBLE;
+
+ALTER TABLE `dcdl`.`t_game` 
+ADD CONSTRAINT `id_league_fk_game`
+  FOREIGN KEY (`id_league_fk`)
+  REFERENCES `dcdl`.`t_league` (`id_league`),
+ADD CONSTRAINT `id_creator_fk`
+  FOREIGN KEY (`creator`)
+  REFERENCES `dcdl`.`t_player` (`id_player`),
+ADD CONSTRAINT `id_winner_fk`
+  FOREIGN KEY (`winner`)
+  REFERENCES `dcdl`.`t_player` (`id_player`),  
+ADD CONSTRAINT `id_loser_fk`
+  FOREIGN KEY (`loser`)
+  REFERENCES `dcdl`.`t_player` (`id_player`);
+
+
+ALTER TABLE `dcdl`.`t_draw` 
+ADD INDEX `id_draw_idx` (`id_draw` ASC) VISIBLE;
+
+ALTER TABLE `dcdl`.`t_solution` 
+ADD INDEX `id_solution_idx` (`id_solution` ASC) VISIBLE;
+
+ALTER TABLE `dcdl`.`t_solution` 
+ADD CONSTRAINT `id_player_fk_solution`
+  FOREIGN KEY (`id_player_fk`)
+  REFERENCES `dcdl`.`t_player` (`id_player`),
+ADD CONSTRAINT `id_draw_fk_solution`
+  FOREIGN KEY (`id_draw_fk`)
+  REFERENCES `dcdl`.`t_draw` (`id_draw`);
+
+ALTER TABLE `dcdl`.`t_solution_detail` 
+ADD INDEX `id_solution_detail_idx` (`id_solution_detail` ASC) VISIBLE;
+
+ALTER TABLE `dcdl`.`t_solution_detail` 
+ADD CONSTRAINT `id_solution_fk_detail`
+  FOREIGN KEY (`id_solution_fk`)
+  REFERENCES `dcdl`.`t_solution` (`id_solution`);
+
+-- association table between games and draws ==> what draws does a game contain?
+ALTER TABLE `dcdl`.`t_game_draws` 
+ADD INDEX `id_game_draw_idx` (`id_game_draw` ASC) VISIBLE;
+
+ALTER TABLE `dcdl`.`t_game_draws` 
+ADD CONSTRAINT `id_game_fk_draws`
+  FOREIGN KEY (`id_game_fk`)
+  REFERENCES `dcdl`.`t_game` (`id_game`),
+ADD CONSTRAINT `id_draw_fk`
+  FOREIGN KEY (`id_draw_fk`)
+  REFERENCES `dcdl`.`t_draw` (`id_draw`);
+
+
+-- association table between games and players ==> what players are playing in the game?
+ALTER TABLE `dcdl`.`t_game_players` 
+ADD INDEX `id_game_player_idx` (`id_game_player` ASC) VISIBLE;
+
+ALTER TABLE `dcdl`.`t_game_players` 
+ADD CONSTRAINT `id_game_fk_players`
+  FOREIGN KEY (`id_game_fk`)
+  REFERENCES `dcdl`.`t_game` (`id_game`),
+ADD CONSTRAINT `id_player_fk`
+  FOREIGN KEY (`id_player_fk`)
+  REFERENCES `dcdl`.`t_player` (`id_player`);
