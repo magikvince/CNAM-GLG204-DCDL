349bbabe73701d8e53c70aa8b728ec8053d0f892
magikvince <vincent1603@hotmail.com>
2022-09-14 23:31:01 +0200

    added a collection of League in model in GameController createGameGET method

--- src/main/java/fr/magikvince/dcdl/game/play/GameController.java
+++ src/main/java/fr/magikvince/dcdl/game/play/GameController.java
@@ -1,14 +1,22 @@
 package fr.magikvince.dcdl.game.play;
 
+import java.util.Collection;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import fr.magikvince.dcdl.game.league.League;
+import fr.magikvince.dcdl.game.league.LeagueService;
+
 
 @Controller
 public class GameController {
+	
+	@Autowired
+	LeagueService leagueService;
 
 	@GetMapping("/games")
 	public String gameGET(Model model)
@@ -25,12 +33,20 @@ public class GameController {
 	@GetMapping("/create-game")
 	public String createGameGET(Model model)
 	{
+		Game newGame = new Game();
+		model.addAttribute("newGame", newGame);
+		
+		Collection<League> leagues = leagueService.findAllLeagues();
+		model.addAttribute("leagues", leagues);
+		
 		return "game/create-game.html";
 	}
 	
 	@PostMapping("/create-game")
 	public String createGamePOST(Model model)
 	{
+		
+		
 		return "game/create-game.html";
 	}
 	
